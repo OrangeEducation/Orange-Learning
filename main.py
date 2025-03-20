e2fc595dc043d35c120b7ddfb23a1e32899a3cf9
OrangeEducation <orangemsonia@gmail.com>
2025-03-20 17:58:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ mame = st.text_input("Enter your name:")
 
 # Displaying a message when a button is clicked
 if st.button("Submit"):
-st.write(f"Hello, {name}! Welcome to Streamlit.")
+  st.write(f"Hello, {name}! Welcome to Streamlit.")
